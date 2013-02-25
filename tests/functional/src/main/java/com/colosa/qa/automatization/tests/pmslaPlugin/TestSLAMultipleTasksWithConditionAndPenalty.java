6d81897ec7d406f7bda41fb712a62f8cbb6dedc1
Ernesto Vega <ernesto.vegap@outlook.com>
2013-02-25 08:59:04 -0400

    Added tests for PMSLA Plugin
    Added files of tests on director java/com/colosa/qa/automatization/pmslaPlugin

---
+++ tests/functional/src/main/java/com/colosa/qa/automatization/tests/pmslaPlugin/TestSLAMultipleTasksWithConditionAndPenalty.java
@@ -0,0 +1,63 @@
+package com.colosa.qa.automatization.tests.pmslaPlugin;
+
+import org.junit.Assert;
+import org.junit.AfterClass;
+import org.junit.Test;
+import java.util.*;
+import com.colosa.qa.automatization.pages.*;
+import com.colosa.qa.automatization.common.*;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class TestSLAMultipleTasksWithConditionAndPenalty{
+
+	protected static int numCase;
+	@Test
+	public void runProcess()throws FileNotFoundException, IOException, Exception{
+
+		Pages.Login().gotoUrl();
+		Pages.Login().loginUser("admin", "admin", "workflow");
+		Pages.Main().goHome();	
+		numCase = Pages.Home().startCase("SLA Process - Multiple Task with Condition and Penalty (Requerimiento de Personal)");
+		Pages.DynaformExecution().intoDynaform();
+		Pages.DynaformExecution().setFieldValue("eleccion", "first");
+		Pages.DynaformExecution().setFieldValue("save", "");
+		Assert.assertTrue(Pages.InputDocProcess().continuebtn());
+
+		Pages.Home().openCase(numCase);
+		Pages.DynaformExecution().intoDynaform();
+		Pages.DynaformExecution().setFieldValue("save", "");
+		Assert.assertTrue(Pages.InputDocProcess().continuebtn());
+
+		Pages.Home().openCase(numCase);
+		Pages.DynaformExecution().intoDynaform();
+		Pages.OutputDocProcess().downloadDocFile();
+		//Pages.OutputDocProcess().downloadPdfFile();
+		Pages.DynaformExecution().setFieldValue("NEXT_STEP", "");
+		Assert.assertTrue(Pages.InputDocProcess().continuebtn());
+
+		Pages.Home().openCase(numCase);
+		Pages.DynaformExecution().intoDynaform();
+		Pages.DynaformExecution().setFieldValue("finalizacion", "");
+		Assert.assertTrue(Pages.InputDocProcess().continuebtn());
+
+		Pages.CronExecute().execute("workflow");
+
+		Pages.Login().gotoUrl();
+		Pages.Login().loginUser("admin", "admin", "");
+		Pages.Main().goHome();
+		Pages.Home().gotoReports();
+
+		Pages.PmslaReport().generateReport("SLA Process - Multiple Task with Condiion and Pen", "All", "All", "All");
+		String[] caseInfo = Pages.PmslaReport().displayCases("SLA Process - Multiple Task with Condiion and Pen");
+		Assert.assertEquals(caseInfo[5], "OPEN");
+		Pages.PmslaReport().displayTasks(numCase);
+		String[] taskInfo = Pages.PmslaReport().getTaskInfo("Documento Generado");
+		Assert.assertEquals(taskInfo[5], "OPEN");
+	
+	}
+
+}
