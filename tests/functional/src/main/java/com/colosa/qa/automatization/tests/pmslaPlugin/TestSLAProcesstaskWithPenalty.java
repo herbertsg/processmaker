3d61a6310c8c8d7f5f9ba0f835d9e6811ea8acd5
Ademar Hurtado <ademar.hurtado@gmail.com>
2013-03-04 15:26:05 -0400

    add tests for pmsla plugin

---
+++ tests/functional/src/main/java/com/colosa/qa/automatization/tests/pmslaPlugin/TestSLAProcesstaskWithPenalty.java
@@ -0,0 +1,38 @@
+package com.colosa.qa.automatization.tests.pmslaPlugin;
+
+import org.junit.Assert;
+import org.junit.AfterClass;
+import org.junit.Test;
+
+import com.colosa.qa.automatization.pages.*;
+import com.colosa.qa.automatization.common.*;
+import org.openqa.selenium.WebElement;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class TestSLAProcesstaskWithPenalty{
+
+	protected static int caseNum;
+
+	@Test
+	public void executeSLAProcesstaskWithPenalty() throws FileNotFoundException, IOException, Exception{
+
+		Pages.Login().gotoUrl();
+		Pages.Login().loginUser("admin", "admin", "workflow");
+		Pages.Main().goHome();	
+		caseNum = Pages.Home().startCase("SLA Process - Task whit penalty (Reclamo)");
+		Pages.DynaformExecution().intoDynaform();
+		Pages.DynaformExecution().setFieldValue("text", "123456");
+		Pages.DynaformExecution().setFieldValue("enviar", "");
+		Assert.assertTrue(Pages.InputDocProcess().continuebtn());
+
+		Pages.CronExecute().execute("workflow");
+
+
+}
+
+}
