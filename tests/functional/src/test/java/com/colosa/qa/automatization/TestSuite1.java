8e2e22d577001bbe7775dba2d53f5673bd4d50d8
Herbert Saal <herbert.saal@colosa.com>
2013-03-06 16:20:33 -0400

    Adiciona ejemplo de suite tests

---
+++ tests/functional/src/test/java/com/colosa/qa/automatization/TestSuite1.java
@@ -0,0 +1,18 @@
+package com.colosa.qa.automatization;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: herbert
+ * Date: 3/4/13
+ * Time: 2:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(value = Suite.class)
+@SuiteClasses(value = { com.colosa.qa.automatization.tests.dependentFields.TestDependentFields.class })
+public class TestSuite1 {
+
+}
