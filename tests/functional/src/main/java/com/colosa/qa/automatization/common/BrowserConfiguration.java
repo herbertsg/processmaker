e21f0996d7b2aaef2a1975f83a1d22d89f5dcd3f
Herbert Saal <herbert.saal@colosa.com>
2013-03-12 10:58:53 -0400

    Add registry class to set browser configuration
    Add maximize browser functionality

---
+++ tests/functional/src/main/java/com/colosa/qa/automatization/common/BrowserConfiguration.java
@@ -0,0 +1,98 @@
+package com.colosa.qa.automatization.common;
+
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Herbert Saal
+ * Date: 3/8/13
+ * Time: 5:16 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BrowserConfiguration {
+    private static BrowserConfiguration ourInstance = new BrowserConfiguration();
+
+    public static BrowserConfiguration getInstance() {
+        return ourInstance;
+    }
+
+    private BrowserConfiguration() {
+    }
+
+    public String getBrowserMode() throws IOException {
+        //read configuration from registry if not found read from configuration file and store in registry
+        String browserMode = (String)Registry.getInstance().getReference("browser.mode");
+        if(browserMode == null){
+            browserMode = ConfigurationSettings.getInstance().getSetting("browser.mode");
+            Registry.getInstance().register("browser.mode", browserMode);
+        }
+
+        return browserMode;
+
+    }
+
+    public void setBrowserMode(String browserMode) throws IOException {
+        //set configuration in registry
+        Registry.getInstance().register("browser.mode", browserMode);
+    }
+
+    public String getBrowserName() throws IOException {
+        String browserName = (String)Registry.getInstance().getReference("browser.name");
+        if(browserName == null){
+            browserName = ConfigurationSettings.getInstance().getSetting("browser.name");
+            Registry.getInstance().register("browser.name", browserName);
+        }
+
+        return browserName;
+    }
+
+    public void setBrowserName(String browserName) throws IOException {
+        //set configuration in registry
+        Registry.getInstance().register("browser.name", browserName);
+    }
+
+    public String getBrowserVersion() throws IOException {
+        String browserVersion = (String)Registry.getInstance().getReference("browser.version");
+        if(browserVersion == null){
+            browserVersion = ConfigurationSettings.getInstance().getSetting("browser.version");
+            Registry.getInstance().register("browser.version", browserVersion);
+        }
+
+        return browserVersion;
+    }
+
+    public void setBrowserVersion(String browserVersion) throws IOException {
+        //set configuration in registry
+        Registry.getInstance().register("browser.version", browserVersion);
+    }
+
+    public String getBrowserPlatform() throws IOException {
+        String browserPlatform = (String)Registry.getInstance().getReference("browser.platform");
+        if(browserPlatform == null){
+            browserPlatform = ConfigurationSettings.getInstance().getSetting("browser.platform");
+            Registry.getInstance().register("browser.platform", browserPlatform);
+        }
+        return browserPlatform;
+    }
+
+    public void setBrowserPlatform(String browserPlatform) throws IOException {
+        //set configuration in registry
+        Registry.getInstance().register("browser.platform", browserPlatform);
+    }
+
+    public String getRemoteServerUrl() throws IOException {
+        String remoteServerUrl = (String)Registry.getInstance().getReference("remote.server.url");
+        if(remoteServerUrl == null){
+            remoteServerUrl = ConfigurationSettings.getInstance().getSetting("remote.server.url");
+            Registry.getInstance().register("remote.server.url", remoteServerUrl);
+        }
+
+        return remoteServerUrl;
+    }
+
+    public void setRemoteServerUrl(String remoteServerUrl) throws IOException {
+        //set configuration in registry
+        Registry.getInstance().register("remote.server.url", remoteServerUrl);
+    }
+
+}
--- tests/functional/src/main/java/com/colosa/qa/automatization/pages/Page.java
+++ tests/functional/src/main/java/com/colosa/qa/automatization/pages/Page.java
@@ -1,16 +1,20 @@
 package com.colosa.qa.automatization.pages;
 
-import java.util.concurrent.TimeUnit;
-import com.colosa.qa.automatization.common.*;
+import com.colosa.qa.automatization.common.Browser;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class Page{
 	protected String url;
 	protected String pageTitle;
 
 	public Page() throws FileNotFoundException, IOException{
+        //create the Browser instance with browser configuration set in the registry
+
+        Browser.driver();
+
 		//init implicit wait time
 		Browser.driver().manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
@@ -18,6 +22,44 @@ public class Page{
 		pageTitle = "";	
 		//System.out.println("Page contructor....:" + url); 	
 	}
+
+    /*
+     * Initialize page with the specified browser, the browser is created locally.
+     * @param browserName The browser instance name: "firefox, chrome, internet explorer"
+     * @throws FileNotFoundException
+     * @throws IOException
+
+    public Page(String browserName ) throws FileNotFoundException, IOException{
+        //create the Browser instance with default browser configuration set in the default.conf file
+        Browser.driver(browserName);
+
+        //init implicit wait time
+        Browser.driver().manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+        url = "";
+        pageTitle = "";
+        //System.out.println("Page contructor....:" + url);
+    }
+
+     * Initialize a page creating a remote browser with the specified configuration.
+     * @param browserName The browser identifier: "firefox, chrome, internet explorer"
+     * @param browserVersion The browser version.
+     * @param browserPlatform The browser platform: "WINDOWS, MAC, LINUX"
+     * @param remoteServerUrl The remote server url
+     * @throws FileNotFoundException
+     * @throws IOException
+
+    public Page(String browserName, String browserVersion, String browserPlatform, String remoteServerUrl) throws FileNotFoundException, IOException{
+        //create the remote Browser instance
+        Browser.driver(browserName, browserVersion, browserPlatform, remoteServerUrl);
+
+        //init implicit wait time
+        Browser.driver().manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+        url = "";
+        pageTitle = "";
+        //System.out.println("Page contructor....:" + url);
+    }*/
 
 	public void gotoUrl(){
 		//System.out.println("Page.Goto url:" + url); 
