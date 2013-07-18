d8540dd2949c41b7c9b4103bc0d69f2e1c7fc0c0
Marco Aurelio Avalos <marc_avalos@yahoo.es>
2013-07-18 17:08:18 -0400

    PMFUserInformation

--- tests/functional/src/main/java/com/colosa/qa/automatization/tests/PMFunctions/TestPMFInformationUser.java
+++ tests/functional/src/main/java/com/colosa/qa/automatization/tests/PMFunctions/TestPMFInformationUser.java
@@ -4,6 +4,7 @@ import com.colosa.qa.automatization.common.FieldKeyType;
 import com.colosa.qa.automatization.common.Value;
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.FileNotFoundException;
@@ -16,6 +17,14 @@ public class TestPMFInformationUser extends com.colosa.qa.automatization.tests.c
     public TestPMFInformationUser(String browserName) throws IOException {
         super(browserName);
     }
+    @Before
+    public void setup(){
+
+    }
+    @After
+    public void cleanup(){
+        browserInstance.quit();
+    }
 
     @Test
 	public void executePMFInformationUser() throws FileNotFoundException, IOException, Exception{
@@ -27,7 +36,19 @@ public class TestPMFInformationUser extends com.colosa.qa.automatization.tests.c
 		caseNum = pages.Home().gotoNewCase().startCase("PMFInformationUser (Get Information user)");
 		pages.DynaformExecution().intoDynaform();
 		//Verify results
-		Assert.assertEquals(Value.getValue(browserInstance, FieldKeyType.ID, "form[userInfoGrid][1][username]"), Value.getValue(browserInstance, FieldKeyType.ID, "form[userName]"));
+        Assert.assertEquals("Invalid UserName.", pages.DynaformExecution().getGridFieldValue("userInfoGrid", 1, "username"), pages.DynaformExecution().getFieldValue("userName"));
+        Assert.assertEquals("Invalid UserName.", pages.DynaformExecution().getGridFieldValue("userInfoGrid", 1, "firstname"), pages.DynaformExecution().getFieldValue("firstName"));
+        //Assert.assertEquals("Invalid FirstName", pages.DynaformExecution().getGridFieldValue("userInfoGrid", 1, "firstName"),pages.DynaformExecution().getFieldValue("firstName") );
+        Assert.assertEquals("Invalid LastName", pages.DynaformExecution().getGridFieldValue("userInfoGrid", 1, "lastname"),pages.DynaformExecution().getFieldValue("lastName") );
+        Assert.assertEquals("Invalid Mail", pages.DynaformExecution().getGridFieldValue("userInfoGrid", 1, "mail"),pages.DynaformExecution().getFieldValue("mail") );
+        Assert.assertEquals("Invalid status", pages.DynaformExecution().getGridFieldValue("userInfoGrid", 1, "status"),pages.DynaformExecution().getFieldValue("status") );
+        Assert.assertEquals("Invalid Address", pages.DynaformExecution().getGridFieldValue("userInfoGrid", 1, "address"),pages.DynaformExecution().getFieldValue("address") );
+        Assert.assertEquals("Invalid Phone", pages.DynaformExecution().getGridFieldValue("userInfoGrid", 1, "phone"),pages.DynaformExecution().getFieldValue("phone") );
+        Assert.assertEquals("Invalid Fax", pages.DynaformExecution().getGridFieldValue("userInfoGrid", 1, "fax"),pages.DynaformExecution().getFieldValue("fax") );
+        Assert.assertEquals("Invalid Cellular", pages.DynaformExecution().getGridFieldValue("userInfoGrid", 1, "cellular"),pages.DynaformExecution().getFieldValue("cellular") );
+        Assert.assertEquals("Invalid birthday", pages.DynaformExecution().getGridFieldValue("userInfoGrid", 1, "birthday"),pages.DynaformExecution().getFieldValue("birthday") );
+        /*
+        Assert.assertEquals(Value.getValue(browserInstance, FieldKeyType.ID, "form[userInfoGrid][1][username]"), Value.getValue(browserInstance, FieldKeyType.ID, "form[userName]"));
 		Assert.assertEquals(Value.getValue(browserInstance, FieldKeyType.ID, "form[userInfoGrid][1][firstname]"), Value.getValue(browserInstance, FieldKeyType.ID, "form[firstName]"));
 		Assert.assertEquals(Value.getValue(browserInstance, FieldKeyType.ID, "form[userInfoGrid][1][lastname]"), Value.getValue(browserInstance, FieldKeyType.ID, "form[lastName]"));
 		Assert.assertEquals(Value.getValue(browserInstance, FieldKeyType.ID, "form[userInfoGrid][1][mail]"), Value.getValue(browserInstance, FieldKeyType.ID, "form[mail]"));
@@ -37,17 +58,16 @@ public class TestPMFInformationUser extends com.colosa.qa.automatization.tests.c
 		Assert.assertEquals(Value.getValue(browserInstance, FieldKeyType.ID, "form[userInfoGrid][1][fax]"), Value.getValue(browserInstance, FieldKeyType.ID, "form[fax]"));
 		Assert.assertEquals(Value.getValue(browserInstance, FieldKeyType.ID, "form[userInfoGrid][1][cellular]"), Value.getValue(browserInstance, FieldKeyType.ID, "form[cellular]"));
 		Assert.assertEquals(Value.getValue(browserInstance, FieldKeyType.ID, "form[userInfoGrid][1][birthday]"), Value.getValue(browserInstance, FieldKeyType.ID, "form[birthday]"));
+		*/
 		pages.DynaformExecution().intoDynaform();
-		pages.DynaformExecution().setFieldValue("send", "");
-		Assert.assertTrue("The button Continue does not exit in this form", pages.InputDocProcess().continuebtn());
-		pages.InputDocProcess().switchToDefault();
+        pages.DynaformExecution().clickButton("send");
+		//pages.DynaformExecution().setFieldValue("send", "");
+		//Assert.assertTrue("The button Continue does not exit in this form", pages.InputDocProcess().continuebtn());
+		//pages.InputDocProcess().switchToDefault();
 		pages.Main().logout();
 
 	}
 
-    @After
-    public void cleanup(){
-        browserInstance.quit();
-    }
+
 
 }
