cb1e3cdf2879af7f042f1e4a8523f0905debdae9
Marvin <marvin.furuya@colosa.com>
2013-10-08 15:29:57 -0700

    modificado2(borrar)

--- tests/functional/src/main/java/com/colosa/qa/automatization/tests/PMFunctions/TestPMFCasesList.java
+++ tests/functional/src/main/java/com/colosa/qa/automatization/tests/PMFunctions/TestPMFCasesList.java
@@ -18,35 +18,25 @@ public class TestPMFCasesList extends com.colosa.qa.automatization.tests.common.
 
     @Test
     public void runCase() throws Exception {
-        
         pages.gotoDefaultUrl();
         pages.Login().loginUser("admin","admin","workflow", "English");
-				pages.Main().goHome();
-        
+		pages.Main().goHome();
         int casenumber = pages.Home().gotoNewCase().startCase("Test PMFCaseList (Task 1)");
         pages.DynaformExecution().intoDynaform();
         pages.DynaformExecution().clickButton("SUBMIT");
         pages.AssignTask().pressContinueButton();
-        
         //pages.Home().gotoInbox();
         pages.Home().gotoInbox().openCase(casenumber);
 		Assert.assertTrue("The case does not exist in inbox", pages.Home().existCase(casenumber));
         pages.Home().gotoInbox().openCase(casenumber);
 		 //pages.Home().openCase(casenumber);
-		 pages.DynaformExecution().intoDynaform();
-
-
+		pages.DynaformExecution().intoDynaform();
         String fieldSTATUS = pages.DynaformExecution().getFieldProperty("STATUS","value");
         Integer fieldNAME = Integer.parseInt(pages.DynaformExecution().getFieldProperty("NAME","value"));
-        
         pages.DynaformExecution().outDynaform();
-        
         pages.Home().gotoParticipated();
         Assert.assertTrue("The case does not exist in Participated", pages.Home().existCase(fieldNAME));
         pages.InputDocProcess().switchToDefault();
-        pages.Main().logout();
-        Thread.sleep(10000);
-
     }
     @After
     public void cleanup(){
